2d68f5f0f2d179fc52b7f64c94c30a3b91e57a7a
devopsengineinfo <devopsengineinfo@gmail.com>
2024-10-25 13:02:27 +0200

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/devopsengineinfo/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('devopsengineinfo')
+            node / gitConfigEmail('devopsengineinfo@gmail.com')
         }
     }
     steps {
